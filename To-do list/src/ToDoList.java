90e8d3fafbe22d5f71d28f70052a9ce88f45d988
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-05-17 14:10:37 -0400

    Added methods to add and remove items to the list

--- To-do list/src/ToDoList.java
+++ To-do list/src/ToDoList.java
@@ -1,3 +1,31 @@
-public class ToDoList {
+import java.util.*;
 
+public class ToDoList {
+    public List<String> items;
+
+    public ToDoList() {
+        items = new ArrayList<>();
+    }
+
+    public void add(String item) {
+        items.add(item);
+        System.out.println(item + " added to the list");
+    }
+
+    public void remove(int index) {
+        if (index >=0 && index < items.size()) {
+            items.remove(index);
+            System.out.println(items.get(index) + " removed from the list");
+        } else {
+            System.out.println(items.get(index) + " is not in the list");
+        }
+    }
+
+    public List<String> getItems() {
+        return new ArrayList<>(items);
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
 }
