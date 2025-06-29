df35053539624fd8f7701115892ff973a4a84115
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-06-29 16:04:03 -0400

    Initial commit

---
+++ To-do Web UI/src/main/java/entity/Item.java
@@ -0,0 +1,37 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "Description", nullable = false, length = 45)
+    private String description;
+
+    public Item() {}
+
+    public Item(String description) {
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
