bfe2de7f690d79ae66ff06d603dfbb4eff8a8a7b
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-06-29 16:31:03 -0400

    Copied actions class from last project

---
+++ To-do_Web_UI/src/main/java/entity/Item.java
@@ -0,0 +1,34 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "item", schema = "mydb")
+public class Item {
+    @Id
+    @Column(name = "ID", nullable = false)
+    private Integer id;
+
+    @Column(name = "Description", nullable = false, length = 45)
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
